36d35c26b4087d32748b465b3d3fb27b2a87e9be
Ryan Rho <rho@uber.com>
2016-06-07 21:45:48 -0700

    Write comments

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-export default function fetchVanguardBalance() {
+export default function fetchVanguardBalance(userName, password, accountId, cb) {
+  if (!cb) {
+    return;
+  }
 
+  // TODO Authenticate
+
+  // TODO Fetch HTML in the balance page.
+
+  // TODO handle errors and pass it to callback.
+
+  const balance = {};
+  cb(null, balance);
 }
