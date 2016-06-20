2c5739d28d776f813a6b19805d83d4188f28054d
Ryan Rho <rho@uber.com>
2016-06-20 00:04:07 -0700

    Switch from request to phantomjs to handle client-side JavaScript

--- index.js
+++ index.js
@@ -1,56 +1,6 @@
-import Request from 'request';
-import {LOGIN_PAGE_URL, LOGIN_FORM_URL, DASHBOARD_URL} from './config/links';
-
-const request = Request.defaults({jar: true});
-
-function getLoginPage() {
-  return new Promise((resolve, reject) => {
-    request(LOGIN_PAGE_URL, (err, res, body) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-    });
-
-    resolve();
-  });
-}
-
-function authenticate(userName, password) {
-  return new Promise((resolve, reject) => {
-    request.post(LOGIN_FORM_URL, {
-      form: {
-        USERNAME: userName,
-        PASSWORD: password
-      }
-    }, function login(err, res, body) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      if (res.statusCode !== 302) {
-        reject({message: 'account cannot be found'});
-        return;
-      }
-
-      resolve();
-    });
-  });
-}
-
-function getDashboardPage() {
-  return new Promise((resolve, reject) => {
-    request(DASHBOARD_URL, function fetchDashboardPage(err, res, body) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(body);
-    });
-  });
-}
+import {create as createPhantom} from 'phantom';
+import {setTimeout} from 'timers';
+import {LOGIN_PAGE_URL} from './config/links';
 
 export default async function fetchVanguardBalance(userName, password, accountId, cb) {
   if (!cb) {
@@ -63,19 +13,34 @@ export default async function fetchVanguardBalance(userName, password, accountId
     return;
   }
 
-  let html;
+  let phantom;
 
   try {
-    await getLoginPage();
-    await authenticate(userName, password);
-    html = await getDashboardPage();
+    // Go to login page.
+    phantom = await createPhantom();
+    const loginHomePage = await phantom.createPage();
+    await loginHomePage.open(LOGIN_PAGE_URL);
+
+    // Authenticate.
+    await loginHomePage.evaluate((userNameInput, passwordInput) => {
+      document.getElementById('USER').value = userNameInput;
+      document.getElementById('PASSWORD').value = passwordInput;
+      document.getElementById('login').click();
+    }, userName, password);
+
+    // TODO Remove setTimeout hack
+    setTimeout(() => {
+      loginHomePage.render('page.png');
+      loginHomePage.close();
+      cb(null, {result: 'not implemented yet'});
+      phantom.exit();
+    }, 5000);
+
+    // TODO Handle Security question page.
+
+    // TODO Get the balance.
   } catch (err) {
     cb(err);
-    return;
+    phantom.exit();
   }
-
-  // TODO Parse the HTML into balance.
-  const result = html;
-
-  cb(null, result);
 }
